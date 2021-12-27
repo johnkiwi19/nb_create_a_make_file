40fddd2ad06f121306e42766471c205aa0ad670b
john <John_Mallett@john.com>
2021-12-27 13:15:42 +1300

    Added a function that will list files in a gived dir

--- src/main/java/john/org/create_make_files/ListOfAllFiles.java
+++ src/main/java/john/org/create_make_files/ListOfAllFiles.java
@@ -5,6 +5,11 @@
  */
 package john.org.create_make_files;
 
+import java.io.File;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  *
  * @author john
@@ -12,9 +17,20 @@ package john.org.create_make_files;
 class ListOfAllFiles {
 
     public ListOfAllFiles() {
-    
+
         System.out.println("This will be a list of all the files");
+        // TODO Need a list of files in the Working Directory
+        Set s = listFilesUsingJavaIO(".");
+        System.out.println(s);
+        System.out.println();
+
     }
-    
-    
+
+    public Set<String> listFilesUsingJavaIO(String dir) {
+        return Stream.of(new File(dir).listFiles())
+                .filter(file -> !file.isDirectory())
+                .map(File::getName)
+                .collect(Collectors.toSet());
+    }
+
 }
